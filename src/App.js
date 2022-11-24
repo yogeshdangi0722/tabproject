b1991c195ff14b7846177dc9c0b16bfbc724d109
Yogesh Kumar Dangi <yogeshdangi0722@gmail.com>
2022-11-24 10:35:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [loading,setLoading]=useState(true);
+  const [jobs,setJobs]=useState([]);
+  const [value,setValue] = useState(0);
+
+  const fetchJob = async ()=>{
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  }
+
+  useEffect(()=>{
+    fetchJob();
+  },[]);
+
+  if(loading){
+    return <section className='section loading'>
+    <h2>Loading...</h2>
+    </section>
+  }
+
+  const {company,dates,duties,title}=jobs[value];
+
+  return <section className='section'>
+          <div className='title'>
+            <h1>Experience</h1>
+            <div className='underline'></div>
+          </div>
+
+          <div className='jobs-center'>
+
+      
+              <article className='job-info'>
+              <h3>{title}</h3>
+              <h4>{company}</h4>
+              <p className='job-date'>{dates}</p>
+              {duties.map((duty,index)=>{
+                return(
+                  <div key={index} className='job-desc'>
+                  <FaAngleDoubleRight className='job-icon' />
+                  <p>{duty}</p>
+                  </div>
+                )
+              })}
+              </article>
+          </div>
+    </section>
 }
 
-export default App;
+export default App
+  
